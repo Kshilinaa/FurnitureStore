28abd98ded84a7c99ddba3726f128059a4bd8fa7
Kshilinaa <ksusha_2605@mail.ru>
2024-05-08 13:02:21 +0300

    init

---
+++ FurnitureStore/FurnitureStore/FurnitureStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurnitureStoreApp.swift
+//  FurnitureStore
+//
+//  Created by Ксения Шилина on 07.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct FurnitureStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
